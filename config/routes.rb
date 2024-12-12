26adca785c7b21168f25f02b5bbf4fd24939dc77
sports-fan <lovesports801@gmail.com>
2024-12-12 10:44:18 -0800

    Add a Route for Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
-
   root "articles#index"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
