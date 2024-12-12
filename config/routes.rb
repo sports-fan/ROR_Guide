f975cbeef168aa623385aba3d3b3fc1c97211064
sports-fan <lovesports801@gmail.com>
2024-12-12 10:16:55 -0800

    Implement CRUD operation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "articles#index"
-  
-  get "/articles", to: "articles#index"
+  resources :articles
 end
